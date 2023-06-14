13da5998c11b75147df242445c6e08a525b654bc
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2023-06-14 15:56:37 +0200

    Added multipage support

--- chatgpt_code_review/main.py
+++ chatgpt_code_review/main.py
@@ -1,6 +1,33 @@
-# NOTE: Keeping this so we don't have to redeploy the Streamlit app
-from app import app
+import streamlit as st
+from streamlit_extras.switch_page_button import switch_page
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
-if __name__ == "__main__":
-    app()
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
+
+want_to_contribute = st.button("I want to contribute!")
+if want_to_contribute:
+    switch_page("app")
