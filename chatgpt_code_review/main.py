e1829c764f67d0e56bcb0e648943213988f6f267
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2023-06-15 17:55:21 +0200

    Added comments to better understand the code + template of spawning buttons to select type of review

--- chatgpt_code_review/main.py
+++ chatgpt_code_review/main.py
@@ -1,35 +1,24 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 from chatgpt_code_review.utils import remove_sidebar
+from chatgpt_code_review import about
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="ChatGPT Code Review",
     page_icon="👋",
     initial_sidebar_state="collapsed"
 )
 
 remove_sidebar()
 
-st.write("# Welcome to Streamlit! 👋")
+session_state = st.session_state
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+st.title("ChatGPT Code Review :rocket:")
 
-want_to_contribute = st.button("I want to contribute!")
+with st.expander("About ChatGPT Code Review"):
+    st.markdown(about.about_section, unsafe_allow_html=True)
+    st.write("")
+
+want_to_contribute = st.button("Review my code")
 if want_to_contribute:
     switch_page("app")
--- chatgpt_code_review/pages/app.py
+++ chatgpt_code_review/pages/app.py
@@ -35,71 +35,78 @@ def app():
 
         session_state = st.session_state
 
-        st.title("ChatGPT Code Review :rocket:")
-
-        with st.expander("About ChatGPT Code Review"):
-            st.markdown(about.about_section, unsafe_allow_html=True)
-            st.write("")
+        st.title("Insert credentials 🐱‍💻")
 
         repo_form = forms.RepoForm()
         with st.form("repo_url_form"):
             repo_form.display_form()
 
-        # Check if the API key is valid before proceeding
-        if repo_form.clone_repo_button and not (repo_form.is_github_repo_valid() and repo_form.is_api_key_valid()):
+        # Explanation of the code:
+        # This section ensures that the code does not proceed if the 'clone_repo_button' is not clicked.
+        # Streamlit will still execute the code below, but the data from GitHub won't be rendered or loaded.
+        # When the button is clicked, its value becomes True, indicating that data should be loaded.
+        # However, if the URL or API key are not valid, the code will break.
+        # In such a case, 'st.stop()' is triggered, and the code stops execution.
+        if repo_form.clone_repo_button and not repo_form.is_github_repo_valid() and not repo_form.is_api_key_valid():
             st.stop()
 
-        repo_url, extensions = repo_form.get_form_data()
+        if repo_form.clone_repo_button and repo_form.is_github_repo_valid() and repo_form.is_api_key_valid():
+            st.button("AD-hoc review on selected code")
+            st.button("Overall review")
 
-        analyze_files_form = forms.AnalyzeFilesForm(session_state)
-        with st.form("analyze_files_form"):
-            if repo_form.clone_repo_button or session_state.get("code_files"):
-                if not session_state.get("code_files"):
-                    session_state.code_files = (
-                        repo.list_code_files_in_repository(
-                            repo_url, extensions
-                        )
-                    )
-
-                analyze_files_form.display_form()
-
-        # Analyze the selected files
-        with st.spinner("Analyzing files..."):
-            if session_state.get("analyze_files"):
-                if session_state.get("selected_files"):
-                    recommendations = query.analyze_code_files(
-                        session_state.selected_files
-                    )
-
-                    # Display the recommendations
-                    st.header("Recommendations")
-                    first = True
-                    recommendation_list = []
-                    for rec in recommendations:
-                        if not first:
-                            st.write("---")
-                        else:
-                            first = False
-                        st.subheader(display.escape_markdown(rec["code_file"]))
-                        recommendation = (
-                            rec["recommendation"] or "No recommendations"
-                        )
-                        st.markdown(recommendation)
-                        with st.expander("View Code"):
-                            extension = os.path.splitext(rec["code_file"])[1]
-                            display.display_code(
-                                rec["code_snippet"], extension
-                            )
-                        recommendation_list.append(rec)
-                    if recommendation_list:
-                        session_state.recommendation_list = recommendation_list
-                else:
-                    st.error("Please select at least one file to analyze.")
-                    st.stop()
-
-        st.write("")
-
-        download.download_markdown(session_state.get("recommendation_list"))
+        # repo_url, extensions = repo_form.get_form_data()
+        #
+        # analyze_files_form = forms.AnalyzeFilesForm(session_state)
+        # with st.form("analyze_files_form"):
+        #     print("Dentro l'analyze form")
+        #     if repo_form.clone_repo_button or session_state.get("code_files"):
+        #         if not session_state.get("code_files"):
+        #             session_state.code_files = (
+        #                 repo.list_code_files_in_repository(
+        #                     repo_url, extensions
+        #                 )
+        #             )
+        #
+        #         analyze_files_form.display_form()
+        #
+        # # Analyze the selected files
+        # with st.spinner("Analyzing files..."):
+        #     print("Dentro lo spinner")
+        #     if session_state.get("analyze_files"):
+        #         if session_state.get("selected_files"):
+        #             recommendations = query.analyze_code_files(
+        #                 session_state.selected_files
+        #             )
+        #
+        #             # Display the recommendations
+        #             st.header("Recommendations")
+        #             first = True
+        #             recommendation_list = []
+        #             for rec in recommendations:
+        #                 if not first:
+        #                     st.write("---")
+        #                 else:
+        #                     first = False
+        #                 st.subheader(display.escape_markdown(rec["code_file"]))
+        #                 recommendation = (
+        #                     rec["recommendation"] or "No recommendations"
+        #                 )
+        #                 st.markdown(recommendation)
+        #                 with st.expander("View Code"):
+        #                     extension = os.path.splitext(rec["code_file"])[1]
+        #                     display.display_code(
+        #                         rec["code_snippet"], extension
+        #                     )
+        #                 recommendation_list.append(rec)
+        #             if recommendation_list:
+        #                 session_state.recommendation_list = recommendation_list
+        #         else:
+        #             st.error("Please select at least one file to analyze.")
+        #             st.stop()
+        #
+        # st.write("")
+        #
+        # download.download_markdown(session_state.get("recommendation_list"))
 
 
 if __name__ == "__main__":
